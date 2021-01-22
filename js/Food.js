1552be0ba39b09be0b8545fc21e649e57a74a925
sambhavi3 <73693635+sambhavi3@users.noreply.github.com>
2021-01-22 15:35:04 +0530

    Add files via upload

---
+++ js/Food.js
@@ -0,0 +1,21 @@
+class dustbin
+{
+	constructor()
+	{
+        var foodStock;
+		
+		this.image=loadImage("sprites/milk.png")
+    }
+    display(){
+    
+    var x=80, y=100;
+    imageMode(CENTER);
+    this.image(this.image,720,220,70,70);
+
+    if(this.foodStock=0){
+        for (vari=0)
+    }
+
+}
+    
+}
